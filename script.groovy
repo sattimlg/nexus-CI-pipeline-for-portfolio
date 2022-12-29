0e003dbc2d869b8088e1d45f469ea0b1f86c1fd4
sattimlg <114459692+sattimlg@users.noreply.github.com>
2022-12-29 17:12:57 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,13 +5,13 @@ def buildApp(){
 
 def pushImage(){
     echo 'Building Image ...'
-    sh 'docker build -t 3.109.213.100:8082/sanskriti-portfolio:${BUILD_NUMBER} .'
+    sh "docker build -t 3.109.213.100:8082/sanskriti-portfolio:${BUILD_NUMBER} ."'
 
     echo 'Pushing image to docker hosted rerpository on Nexus'
 
     withCredentials([usernamePassword(credentialsId: 'nexus', passwordVariable: 'PSW', usernameVariable: 'USER')]){
-        sh 'echo ${PSW} | docker login -u ${USER} --password-stdin 3.109.213.100:8082'
-        sh 'docker push 3.109.213.100:8082/sanskriti-portfolio:${BUILD_NUMBER}'
+        sh "echo ${PSW} | docker login -u ${USER} --password-stdin 3.109.213.100:8082"
+        sh "docker push 3.109.213.100:8082/sanskriti-portfolio:${BUILD_NUMBER}"
     }
 }
 
@@ -25,8 +25,8 @@ def deployApp(){
 
 def commitChanges(){
         withCredentials([usernamePassword(credentialsId: 'github', passwordVariable: 'PSW', usernameVariable: 'USER')]) {
-        sh "git config --global user.name jenkins"
-        sh "git config --global user.email my.jenkins.server@gmail.com"
+        sh 'git config --global user.name "jenkins"'
+        sh 'git config --global user.email "my.jenkins.server@gmail.com"'
         sh "git remote set-url origin https://${USER}:${PSW}@github.com/sattimlg/nexus-CI-pipeline-for-portfolio.git"
 
         sh '''
